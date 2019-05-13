c1086982c81bdfd5e0266f6e6dc9c5d39b6db235
Pedro Abdalla <pedroabdalla@outlook.com>
2019-05-12 22:58:19 -0300

    add getters

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
                 <h1>Vuex</h1>
                 <app-result></app-result>
                 <hr>
+                <app-another-result></app-another-result>
+                <hr>
                 <app-counter></app-counter>
             </div>
         </div>
@@ -14,11 +16,13 @@
 <script>
     import Counter from './components/Counter.vue';
     import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
 
     export default {
         components: {
             appCounter: Counter,
             appResult: Result,
+            appAnotherResult: AnotherResult,
         }
     }
 </script>
