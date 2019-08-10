1b9f36eaf4c96b1393af2472697038d82d51f44c
Pedro Abdala <pedroabdalla@outlook.com>
2019-08-09 23:09:52 -0300

    veux actions

--- src/store/store.js
+++ src/store/store.js
@@ -16,11 +16,29 @@ export const store = new Vuex.Store({
         }
     },
     mutations: {
-        increment: state => {
-            state.counter++;
+        increment: (state, payload) => {
+            state.counter += payload;
         },
-        decrement: state => {
-            state.counter--;
+        decrement: (state, payload) => {
+            state.counter -= payload
+        }
+    },
+    actions: {
+        increment: ({ commit }, payload) => {
+            commit('increment', payload)
+        },
+        decrement: ({ commit }, payload) => {
+            commit('decrement', payload)
+        },
+        asyncIncrement: ({ commit }) => {
+            setTimeout(() => {
+                commit('increment')
+            }, 1000);
+        },
+        asyncIncrement: ({ commit }) => {
+            setTimeout(() => {
+                commit('increment')
+            }, 1000);
         }
     }
 })
